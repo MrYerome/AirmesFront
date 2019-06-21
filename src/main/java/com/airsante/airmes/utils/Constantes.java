331613c40d260f25700982dd3fcd7e7d274d43a2
Unknown <vinet.jerome@gmail.com>
2019-06-21 16:52:41 +0200

    Modification du POM

--- src/main/java/com/airsante/airmes/utils/Constantes.java
+++ src/main/java/com/airsante/airmes/utils/Constantes.java
@@ -1,8 +1,10 @@
 package com.airsante.airmes.utils;
 
 public class Constantes {
-
+//Dev
     private static String url = "http://localhost:8090/api/";
+    //Prod
+   // private static String url = "http://localhost:8090/AirmesDao/api";
 
     public static String getUrl() {
         return url;
